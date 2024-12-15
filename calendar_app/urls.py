330944e5fbc43e2e270381774564b5c336c45c0b
Martin Cowey <starman.martin@gmail.com>
2024-12-15 21:51:53 +0000

    Add Django All auth and events app, fixed the errros for home page and url paths

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from .views import CalendarListView, CalendarWindowDetailView, home
 
-urlpatterns = [ 
-    path('',
-        views.HomePage.as_view(), name='home'), 
+urlpatterns = [
+    path('', home, name='home'),  # This sets the home view as the default landing page
+    path('calendar/', CalendarListView.as_view(), name='calendar_list'),
+    path('window/<int:pk>/', CalendarWindowDetailView.as_view(), name='window_detail'),
 ]
--- calendar_app/views.py
+++ calendar_app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-class HomePage(TemplateView): 
-    """
-    Displays home page"
-    """
-    template_name = 'index.html'
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import CalendarWindow
+from django.utils import timezone
+
+# Home view to display events and information about Code Institute
+def home(request):
+    events = CalendarWindow.objects.filter(is_event=True)  # Adjust this query as needed
+    return render(request, 'calendar_app/home.html', {
+        'events': events,
+        'current_year': timezone.now().year,
+    })
+
+
+class CalendarListView(LoginRequiredMixin, ListView):
+    model = CalendarWindow
+    template_name = 'calendar_app/calendar_list.html'
+    context_object_name = 'windows'
+
+
+class CalendarWindowDetailView(LoginRequiredMixin, DetailView):
+    model = CalendarWindow
+    template_name = 'calendar_app/window_detail.html'
+    context_object_name = 'window'
